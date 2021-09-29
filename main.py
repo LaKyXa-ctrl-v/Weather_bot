e0272bbd82b28f61cb0c81a6a317c9a557e05e1d
LaKyXa <lakyxamersh@gmail.com>
2021-09-29 12:45:15 +0300

    full

--- main.py
+++ main.py
@@ -1,34 +1,49 @@
-
-from flask import Flask, request
+from aiogram.types import ReplyKeyboardRemove, ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
+from pprint import pprint
+import requests
 import telebot
-
+import time
+from telebot import types
 bot = telebot.TeleBot('2048875525:AAFwUQ5otU0g_MSsGNoNJvUfnH7RilgWa3I')
-bot.set_webhook(url="https://2749-176-110-46-211.ngrok.io")
-app = Flask(__name__)
-
-
-@app.route('/', methods=["POST"])
-def webhook():
-    bot.process_new_updates(
-        [telebot.types.Update.de_json(request.stream.read().decode("utf-8"))]
-    )
-    return "ok"
 
 
 @bot.message_handler(commands=['start'])
 def start_command(message):
-    bot.send_message(message.chat.id, 'Hello!')
+    bot.send_message(
+        message.chat.id, "Привет,я бот который показывает погоду! Что бы узнать погоду /weather")
 
 
-@bot.message_handler(commands=['play'])
-def start_command(message):
-    bot.send_message(message.chat.id, 'play opa opa')
+sity = None
 
 
-@bot.message_handler(commands=[''])
-def start_command(message):
-    bot.send_message(message.chat.id, 'hmmm')
+@bot.message_handler(commands=['weather'])
+def handle_text(message):
+    bot.send_message(message.chat.id, "Погода в каком городе вас интересует?")
+
+    @bot.message_handler(content_types=['text'])
+    def handle_text(message):
+        try:
+            city = message.text
+            r = requests.get(
+                f'http://api.openweathermap.org/data/2.5/weather?q={city}&APPID=2a1fa51ed2d0b30a5e976b62e76638e3')
+            weather = r.json()
+            temp = int(int(weather["main"]["temp"])-273.15)
+            printweather = f"Погода в городе {city} в данный момент: {temp} градусов"
+            bot.send_message(message.chat.id, printweather)
+        except:
+            bot.send_message(
+                message.chat.id, "Я не нашел ваш город =( Попробуй еще! /weather")
 
 
-if __name__ == "__main__":
-    app.run()
+button_hi = KeyboardButton('Привет! 👋')
+
+greet_kb = ReplyKeyboardMarkup()
+greet_kb.add(button_hi)
+
+while True:
+    try:
+        bot.polling(none_stop=True)
+
+    except Exception as e:
+        print(e)
+        time.sleep(15)
